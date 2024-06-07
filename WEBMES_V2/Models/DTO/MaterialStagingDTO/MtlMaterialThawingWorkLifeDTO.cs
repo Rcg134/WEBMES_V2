374fbda7084122f95508b0ad0ffbba8007b98081
TheReal08 <dasibayan17@gmail.com>
2024-06-07 19:52:28 +0800

    Material Staging DTO
    Add Material Staging DTO

---
+++ WEBMES_V2/Models/DTO/MaterialStagingDTO/MtlMaterialThawingWorkLifeDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace WEBMES_V2.Models.DTO.MaterialStagingDTO
+{
+    public class MtlMaterialThawingWorkLifeDTO
+    {
+        public int ID { get; set; }
+        public int? MaterialType { get; set; }
+        public string? SID { get; set; }
+        public string? ProductType { get; set; }
+        public int? ThawingTime { get; set; }
+        public int? WorkLife { get; set; }
+    }
+}
---
+++ WEBMES_V2/Models/DTO/MaterialStagingDTO/MtlMaterialTrackingDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace WEBMES_V2.Models.DTO.MaterialStagingDTO
+{
+    public class MtlMaterialTrackingDTO
+    {
+        //public int Id { get; set; }
+        public string? Qrcode { get; set; }
+        public string? Sid { get; set; }
+        public string? Batch { get; set; }
+        public string? BatchNumber { get; set; }
+        public DateTime? ThawIn { get; set; }
+        public int? ThawInBy { get; set; }
+        public DateTime? ThawOut { get; set; }
+        public int? ThawOutBy { get; set; }
+        public DateTime? ThawOutEnd { get; set; }
+        public DateTime? WorkLifeEnd { get; set; }
+    }
+}
